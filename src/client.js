844ed9f9d37e6db8bc83f9e5b3fdcd19e07aa2c8
DeepGandhi07 <deepgandhi9887@gmail.com>
2022-11-26 12:29:34 +0530

    Final

---
+++ src/client.js
@@ -0,0 +1,15 @@
+import sanityClint from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = sanityClint({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2022-11-12",
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
